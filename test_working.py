ac80213b0b29d97d54b5cfcebe2aaa120d3cd227
duanniten <duann_@hotmail.com>
2024-09-25 10:36:12 -0300

    create tests

---
+++ test_working.py
@@ -0,0 +1,48 @@
+import pytest
+
+from working import convert
+
+def test_check_minutes():
+    hora = '9:00 AM to 5:00 PM'
+    assert convert(hora) == '09:00 to 17:00' 
+
+def test_check_no_minutes():
+    hora = '9 AM to 5 PM'
+    assert convert(hora) == '09:00 to 17:00' 
+
+def test_check_first_minutes():
+    hora = '9:15 AM to 5 PM'
+    assert convert(hora) == '09:15 to 17:00' 
+
+def test_check_last_minutes():
+    hora = '9 AM to 5:15 PM'
+    assert convert(hora) == '09:00 to 17:15' 
+
+def test_first_pm():
+    hora = '9 PM to 5:15 AM'
+    assert convert(hora) == '21:00 to 05:15' 
+
+def test_invalid_minutes():
+    hora = '9:60 AM to 5:00 PM'
+    with pytest.raises(ValueError):
+        convert(hora)
+    
+    hora = '9:00 AM to 5:60 PM'
+    with pytest.raises(ValueError):
+        convert(hora)
+
+def test_invalid_hour():
+    hora = '13:00 AM to 5:00 PM'
+    with pytest.raises(ValueError):
+        convert(hora)
+    
+    hora = '9:00 PM to 13:00 AM'
+    with pytest.raises(ValueError):
+        convert(hora)
+
+def test_invalid_hour():
+    hora = '09:00 AM - 5:00 PM'
+    with pytest.raises(ValueError):
+        convert(hora)
+
+        
---
+++ working.py
@@ -0,0 +1,51 @@
+import re
+import sys
+
+
+def main():
+    print(convert(input("Hours: ")))
+
+
+def convert(s):
+    if  matches := re.search(
+                            r'([0-9][0-9]?):?([0-9][0-9]?)?.+(AM|PM) +to +'
+                            r'([0-9][0-9]?):?([0-9][0-9]?)?.+(AM|PM)',
+                            s):
+        hour_start, minute_strart, meridiem_start, hour_fin, minute_fin, meridiem_fin = matches.groups() 
+
+        if 0 < int(hour_start) > 12 or 0 < int(hour_fin) > 12:
+            raise ValueError
+        
+        if minute_strart!= None and int(minute_strart) > 59:
+            raise ValueError
+        
+        if minute_fin!= None and int(minute_fin) > 59:
+            raise ValueError
+
+        if meridiem_start == 'PM':
+            hour_start = int(hour_start) + 12
+        else:
+            hour_start = int(hour_start)
+
+        if meridiem_fin == 'PM':
+            hour_fin = int(hour_fin) + 12
+        else:
+            hour_fin = int(hour_fin)
+
+        if minute_strart == None:
+            minute_strart = '0'
+        
+        if minute_fin == None:
+            minute_fin = '0'
+
+        minute_strart = int(minute_strart)
+        minute_fin = int(minute_fin)
+
+        final_string = f'{hour_start:02}:{minute_strart:02} to {hour_fin:02}:{minute_fin:02}'
+
+        return final_string
+    else:
+        raise ValueError
+
+if __name__ == "__main__":
+    main()
